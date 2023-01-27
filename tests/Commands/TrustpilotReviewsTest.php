7a1efe395e49d0002a6a0507ebd3045ec6ad4eed
alex <aleksandar.naydenov@delta.bg>
2023-01-27 19:31:17 +0200

    Added Command tests.

---
+++ tests/Commands/TrustpilotReviewsTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use function Pest\Laravel\artisan;
+
+it('returns TrustPilot reviews', function () {
+    artisan('trustpilot:reviews')->assertSuccessful();
+});
+
+it('returns TrustPilot reviews for domain', function () {
+    artisan('trustpilot:reviews google.com')->assertSuccessful();
+});
+
+it('returns TrustPilot reviews for domain and language', function () {
+    artisan('trustpilot:reviews google.com bg')->assertSuccessful();
+});
+
+it('returns TrustPilot domain error', function () {
+    artisan('trustpilot:reviews not_a_domain')
+        ->expectsOutputToContain("TrustPilot reviews couldn't be retrieved!")
+        ->expectsOutputToContain('Error:')
+        ->assertFailed();
+});
---
+++ tests/Commands/TrustpilotScoreTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use function Pest\Laravel\artisan;
+
+it('returns TrustPilot score', function () {
+    artisan('trustpilot:score')
+        ->expectsOutputToContain('TrustPilot score ->')
+        ->assertSuccessful();
+});
+
+it('returns TrustPilot score for domain', function () {
+    artisan('trustpilot:score google.com')
+        ->expectsOutputToContain('TrustPilot score ->')
+        ->assertSuccessful();
+});
+
+it('returns TrustPilot error', function () {
+    artisan('trustpilot:score asd')
+        ->expectsOutputToContain("TrustPilot score couldn't be retrieved!")
+        ->expectsOutputToContain('Error:')
+        ->assertFailed();
+});
